f18627fcd473224cceb33b386c651058b4605e48
sahidul7 <mdsahidulislam4047@gmail.com>
2025-05-17 15:09:52 +0600

    Route demo for profile and about page

---
+++ route-demo/src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function about () {
+  return (
+    <div>
+      <h1>Route Demo for About Page</h1>
+      <p > <h2>Welcome from about page !</h2></p>
+    </div>
+  );
+}   
---
+++ route-demo/src/app/profile/page.tsx
@@ -0,0 +1,8 @@
+export default function profile () {
+  return (
+    <div>
+      <h1>Route Demo for Profile Page</h1>
+      <p > <h2>Welcome from profile page !</h2></p>
+    </div>
+  );
+}
